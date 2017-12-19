cc33d1ae7be5d12924a117da0baecd4a133b808c
Ryan Winchester <code@ryanwinchester.ca>
2017-12-19 00:12:38 -0800

    day3

--- 2017/day03.exs
+++ 2017/day03.exs
@@ -1,7 +1,9 @@
 defmodule Day03 do
   @moduledoc """
   --- Day 3: Spiral Memory ---
+  """
 
+  @doc """
   You come across an experimental new kind of memory stored on an infinite
   two-dimensional grid.
 
@@ -32,8 +34,7 @@ defmodule Day03 do
   How many steps are required to carry the data from the square identified in
   your puzzle input all the way to the access port?
   """
-
-  def steps_from(x) do
+  def part1(x) do
     matrix = get_n(x) |> build_matrix()
     {x0, y0} = Map.get(matrix, 1)
     {x, y} = Map.get(matrix, x)
@@ -55,12 +56,44 @@ defmodule Day03 do
   defp cell(n, x, i, j) when i <= j, do: (n-2*x)*(n-2*x) - (i-x) - (j-x)
 
   defp cell(n, x, i, j), do: (n-2*x-2)*(n-2*x-2) + (i-x) + (j-x)
+
+  @doc """
+  As a stress test on the system, the programs here clear the grid and then
+  store the value 1 in square 1. Then, in the same allocation order as shown
+  above, they store the sum of the values in all adjacent squares, including
+  diagonals.
+
+  So, the first few squares' values are chosen as follows:
+
+  - Square 1 starts with the value 1.
+  - Square 2 has only one adjacent filled square (with value 1), so it also
+    stores 1.
+  - Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
+  - Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
+  - Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
+
+  Once a square is written, its value does not change. Therefore, the first few squares would receive the following values:
+
+      147  142  133  122   59
+      304    5    4    2   57
+      330   10    1    1   54
+      351   11   23   25   26
+      362  747  806--->   ...
+
+  What is the first value written that is larger than your puzzle input?
+  """
+  def part2(x) do
+  end
 end
 
 
-## Test
+## Test - Part 1
+IO.puts "--- part 1 ---"
+IO.puts Day03.part1(1) == 0
+IO.puts Day03.part1(12) == 3
+IO.puts Day03.part1(23) == 2
+IO.puts Day03.part1(1024) == 31
 
-IO.puts Day03.steps_from(1) == 0
-IO.puts Day03.steps_from(12) == 3
-IO.puts Day03.steps_from(23) == 2
-IO.puts Day03.steps_from(1024) == 31
+## Test - Part 2
+IO.puts "--- part 2 ---"
+IO.inspect Day03.part2()
